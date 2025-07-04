101b3322832d72e949afbf79fb8a4384f47ef348
Eduardo Miranda <eduardo.miranda@austranet.com>
2025-07-04 03:27:42 -0400

    Eliminación de dockerización

--- etl_api.py
+++ etl_api.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
+from main import run_etl
 
 app = FastAPI()
 scheduler = BackgroundScheduler()
 scheduler.start()
 
-def run_etl_job():
-    from main import run_etl
-    run_etl()
-
 @app.on_event("startup")
 def schedule_etl():
-    run_etl_job()
-    scheduler.add_job(run_etl_job, CronTrigger(hour=0, minute=0), id="daily_etl", replace_existing=True)
+    run_etl()
+    scheduler.add_job(run_etl, CronTrigger(hour=0, minute=0), id="daily_etl", replace_existing=True)
